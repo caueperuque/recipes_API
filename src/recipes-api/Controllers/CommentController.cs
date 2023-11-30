06d027562189bb1d7fd2f16f196f8da8b0272cc5
Caue Peruque <cauepptrybe@gmail.com>
2023-11-30 10:08:22 -0300

    finished project

---
+++ src/recipes-api/Controllers/CommentController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using recipes_api.Services;
+using recipes_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace recipes_api.Controllers;
+
+[ApiController]
+[Route("comment")]
+public class CommentController : ControllerBase
+{  
+    public readonly ICommentService _service;
+    
+    public CommentController(ICommentService service)
+    {
+        this._service = service;        
+    }
+
+    // 10 - Sua aplicação deve ter o endpoint POST /comment
+    [HttpPost]
+    public IActionResult Create([FromBody]Comment comment)
+    {
+        _service.AddComment(comment);
+        return Created("Created comment", comment);
+    }
+
+    // 11 - Sua aplicação deve ter o endpoint GET /comment/:recipeName
+    [HttpGet("{name}", Name = "GetComment")]
+    public IActionResult Get(string name)
+    {                
+        var comment = _service.GetComments(name);
+        return Ok(comment);                 
+    }
+}
---
+++ src/recipes-api/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Mvc;
+using recipes_api.Services;
+using recipes_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace recipes_api.Controllers;
+
+[ApiController]
+[Route("user")]
+public class UserController : ControllerBase
+{
+    public readonly IUserService _service;
+
+    public UserController(IUserService service)
+    {
+        this._service = service;
+    }
+
+    // 6 - Sua aplicação deve ter o endpoint GET /user/:email
+    [HttpGet("{email}", Name = "GetUser")]
+    public IActionResult Get(string email)
+    {
+        var user = _service.GetUser(email);
+        if (user is null) return NotFound();
+        return Ok(user);
+    }
+
+    // 7 - Sua aplicação deve ter o endpoint POST /user
+    [HttpPost]
+    public IActionResult Create([FromBody] User user)
+    {
+        _service.AddUser(user);
+        return Created("Created user", user);
+    }
+
+    // "8 - Sua aplicação deve ter o endpoint PUT /user
+    [HttpPut("{email}")]
+    public IActionResult Update(string email, [FromBody] User user)
+    {
+        try
+        {
+            var userExist = _service.GetUser(email);
+            if (userExist is null)
+            {
+                return NotFound();
+            }
+            _service.UpdateUser(user);
+            return Ok();
+        }
+        catch
+        {
+            return BadRequest();
+        }
+    }
+
+    // 9 - Sua aplicação deve ter o endpoint DEL /user
+    [HttpDelete("{email}")]
+    public IActionResult Delete(string email)
+    {
+        var userExist = _service.GetUser(email);
+        if (userExist is null)
+        {
+            return NotFound();
+        }
+
+        _service.DeleteUser(email);
+        return NoContent();
+    }
+}
